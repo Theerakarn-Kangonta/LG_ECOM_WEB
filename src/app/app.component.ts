e886cfc74aec10850216e6cb005c8ef06a715f16
Theerakarn-Kangonta <theerakarn.kangonta@gmail.com>
2025-05-02 10:20:20 +0700

    SNAP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `
-    <h1>Mini Blog (Standalone SSR)</h1>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { PostListComponent } from './app/components/post-list/post-list.component';
 import { provideHttpClient } from '@angular/common/http';
+import { HomeComponent } from './app/components/home/home.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter([
-      { path: '', component: PostListComponent }
+      { path: '', component: HomeComponent }
     ]),
     provideHttpClient() // <-- Important for HttpClient!
   ]
